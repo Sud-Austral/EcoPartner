b7e9430a870483122f72ed96869386943f8f5e02
lmonsalve <lmonsalve22@gmail.com>
2020-09-02 11:21:13 -0400

    AplicacionBase

---
+++ AplicacionLogin/AplicacionLogin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AplicacionLogin.Startup))]
+namespace AplicacionLogin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
