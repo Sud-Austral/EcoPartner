a1f3c48c478cc55ef7543d159dc93765a35c3472
lmonsalve <lmonsalve22@gmail.com>
2020-09-02 11:35:27 -0400

    controladores

---
+++ AplicacionLogin/AplicacionLogin/Controllers/CamionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionLogin.Controllers
+{
+    public class CamionController : Controller
+    {
+        // GET: Camion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
