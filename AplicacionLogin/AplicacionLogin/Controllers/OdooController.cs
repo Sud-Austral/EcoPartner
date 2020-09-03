d0c680769408011212859f676be6d262942ff3c6
lmonsalve <lmonsalve22@gmail.com>
2020-09-03 00:05:30 -0400

    odoo

---
+++ AplicacionLogin/AplicacionLogin/Controllers/OdooController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionLogin.Controllers
+{
+    public class OdooController : Controller
+    {
+        // GET: Odoo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
