4c4c4fcae9790bd46eedcfca8fb0387cea87e627
Viviandrg7 <viviandrg7@gmail.com>
2020-12-08 01:18:04 -0600

    weboay

--- AplicacionLogin/AplicacionLogin/Controllers/AutoController.cs
+++ AplicacionLogin/AplicacionLogin/Controllers/AutoController.cs
@@ -8,7 +8,6 @@ using System.Web;
 using System.Web.Mvc;
 using Transbank.Webpay;
 
-
 namespace AplicacionLogin.Controllers
 {
     public class AutoController : Controller
@@ -91,10 +90,11 @@ namespace AplicacionLogin.Controllers
             var orden = Guid.NewGuid().ToString().Replace("-", string.Empty).Substring(0, 8);
             var id = Guid.NewGuid().ToString().Replace("-", string.Empty).Substring(0, 8);
 
-           //string returnUrl = "http://localhost:62106/Auto/Retorno";
-           //string returnFinal = "http://localhost:62106/Auto/Final";
-           string returnUrl = "https://ecopartnerbank.azurewebsites.net/Auto/Retorno";
-           string returnFinal = "https://ecopartnerbank.azurewebsites.net/Auto/Final";
+           string returnUrl = "http://localhost:62106/Auto/Retorno";
+           string returnFinal = "http://localhost:62106/Auto/Final";
+           //string returnUrl = "https://ecopartnerbank.azurewebsites.net/Auto/Retorno";
+           //string returnFinal = "https://ecopartnerbank.azurewebsites.net/Auto/Final";
+
             int montotrans = Convert.ToInt32(calculo * 800); 
             var initResult = transaction.initTransaction(montotrans, orden, id, returnUrl, returnFinal);
 
