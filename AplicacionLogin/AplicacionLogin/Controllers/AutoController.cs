182135dac342bcc771e863798f8e6c3e70933c79
Mario9191 <mariosalazar9191@gmail.com>
2020-12-18 19:41:57 -0300

    Mostrar datos en vista Final

--- AplicacionLogin/AplicacionLogin/Controllers/AutoController.cs
+++ AplicacionLogin/AplicacionLogin/Controllers/AutoController.cs
@@ -40,7 +40,7 @@ namespace AplicacionLogin.Controllers
 
 
 
-        public ActionResult auto_pagina4(double porsentaje, double total, double ton, string nombre, string correo)
+        public ActionResult auto_pagina4(double porsentaje, double total, double ton, string nombre, string correo, string telefono, string empresa, string pais)
         {
             ViewBag.Title = "Compensación de carbono para Autos";
             CALCULOS ca = new CALCULOS();
@@ -50,6 +50,8 @@ namespace AplicacionLogin.Controllers
             ViewBag.totalf = totalf;
             ViewBag.nombre = nombre;
             ViewBag.correo = correo;
+            
+
             return View();
         }
         public ActionResult indexauto()
@@ -58,30 +60,37 @@ namespace AplicacionLogin.Controllers
             return View();
         }
 
-        public ActionResult auto_pagina4_1(double calculo, double ton)
+        public ActionResult auto_pagina4_1(double calculo, double ton, string nombre, string email, string telefono, string empresa, string pais)
         {
             ViewBag.Title = "Compensación de carbono para Autos";
             ViewBag.total = calculo;
             ViewBag.toneladas = ton;
+            Session["nombre"] = nombre;
+            Session["telefono"] = telefono;
+            Session["empresa"] = empresa;
+            Session["pais"] = pais;
+            Session["correo"] = email;
             Session["toneladas"] = ton;
+            Session["total"] = calculo;
+            
 
 
 
             //*********************************************************************************
             //                                     Ambiente de producción
             //*********************************************************************************
-            var configuration = new Configuration();
-            configuration.Environment = "PRODUCCION";
-            configuration.CommerceCode = "597036300078";
-            configuration.PrivateCertPfxPath = @"D:\home\site\wwwroot\Content\Certificados\597036300078.pfx";
+            //var configuration = new Configuration();
+            //configuration.Environment = "PRODUCCION";
+            //configuration.CommerceCode = "597036300078";
+            //configuration.PrivateCertPfxPath = @"D:\home\site\wwwroot\Content\Certificados\597036300078.pfx";
            
-            configuration.Password = "a";
-            configuration.WebpayCertPath = Configuration.GetProductionPublicCertPath();
-            var transaction = new Webpay(configuration).NormalTransaction;    //.NormalTransaction;
+            //configuration.Password = "a";
+            //configuration.WebpayCertPath = Configuration.GetProductionPublicCertPath();
+            //var transaction = new Webpay(configuration).NormalTransaction;    //.NormalTransaction;
             //*********************************************************************************
             //                                     Ambiente de prueba
             //*********************************************************************************
-           //var transaction = new Webpay(Configuration.ForTestingWebpayPlusNormal()).NormalTransaction;
+           var transaction = new Webpay(Configuration.ForTestingWebpayPlusNormal()).NormalTransaction;
 
             //Convert.ToInt16(calculo);
 
@@ -91,10 +100,10 @@ namespace AplicacionLogin.Controllers
             var orden = Guid.NewGuid().ToString().Replace("-", string.Empty).Substring(0, 8);
             var id = Guid.NewGuid().ToString().Replace("-", string.Empty).Substring(0, 8);
 
-       //   string returnUrl = "http://localhost:62106/Auto/Retorno";
-         // string returnFinal = "http://localhost:62106/Auto/Final";
-           string returnUrl = "https://ecopartnerbank.azurewebsites.net/Auto/Retorno";
-           string returnFinal = "https://ecopartnerbank.azurewebsites.net/Auto/Final";
+            string returnUrl = "http://localhost:62106/Auto/Retorno";
+            string returnFinal = "http://localhost:62106/Auto/Final";
+           //string returnUrl = "https://ecopartnerbank.azurewebsites.net/Auto/Retorno";
+           //string returnFinal = "https://ecopartnerbank.azurewebsites.net/Auto/Final";
 
             int montotrans = Convert.ToInt32(calculo * 800);
             var initResult = transaction.initTransaction(montotrans, orden, id, returnUrl, returnFinal);
@@ -172,7 +181,14 @@ namespace AplicacionLogin.Controllers
             ViewBag.Message = "Your contact page.";
             //ViewBag.toneladas = Response.Write Session("toneladas");
             //var ton = Session["toneladas"];
+            ViewBag.nombre = Session["nombre"];
+            ViewBag.telefono = Session["telefono"];
+            ViewBag.empresa = Session["empresa"];
+            ViewBag.pais = Session["pais"];
+            ViewBag.correo = Session["correo"];
             ViewBag.tonelada = Session["toneladas"];
+            ViewBag.total = Session["total"];
+            
             //ViewBag.tonelada = ton;
 
 
@@ -205,18 +221,18 @@ namespace AplicacionLogin.Controllers
             //*********************************************************************************
             //                                     Ambiente de producción
             //*********************************************************************************
-            var configuration = new Configuration();
-            configuration.Environment = "PRODUCCION";
-            configuration.CommerceCode = "597036300078";
-            configuration.PrivateCertPfxPath = @"D:\home\site\wwwroot\Content\Certificados\597036300078.pfx";
+            //var configuration = new Configuration();
+            //configuration.Environment = "PRODUCCION";
+            //configuration.CommerceCode = "597036300078";
+            //configuration.PrivateCertPfxPath = @"D:\home\site\wwwroot\Content\Certificados\597036300078.pfx";
 
-            configuration.Password = "a";
-            configuration.WebpayCertPath = Configuration.GetProductionPublicCertPath();
-            var transaction = new Webpay(configuration).NormalTransaction;    //.NormalTransaction;
+            //configuration.Password = "a";
+            //configuration.WebpayCertPath = Configuration.GetProductionPublicCertPath();
+            //var transaction = new Webpay(configuration).NormalTransaction;    //.NormalTransaction;
             //*********************************************************************************
             //                                     Ambiente de prueba
             //*********************************************************************************
-         //   var transaction = new Webpay(Configuration.ForTestingWebpayPlusNormal()).NormalTransaction;
+            var transaction = new Webpay(Configuration.ForTestingWebpayPlusNormal()).NormalTransaction;
           
             
             //Conf.WebpayCertPath = Configuration.GetProductionPublicCertPath();
