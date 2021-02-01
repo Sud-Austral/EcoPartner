67b71307acc621139adf49de1ef1f201c8c8521f
Mario9191 <mariosalazar9191@gmail.com>
2021-02-01 17:36:09 -0300

    ajuste de filtro aeropuerto por codigo de 3 letras

--- AplicacionLogin/AplicacionLogin/Scripts/Datos/Buscar_Datos.js
+++ AplicacionLogin/AplicacionLogin/Scripts/Datos/Buscar_Datos.js
@@ -20,15 +20,17 @@ function mostrarLista(texto, lista) {
 
         $.each(lines, function (n, elem) {
             var col = elem.split(',"');
-            if (col[1] != "" && typeof col[1] != 'undefined' && col[2] != "" && typeof col[2] != 'undefined') {
+            if (col[1] != "" && typeof col[1] != 'undefined' && col[2] != "" && typeof col[2] != 'undefined' && col[4] != "" && typeof col[4] != 'undefined') {
                 var column = col[1].replace('"', '');
                 var column2 = col[2].replace('"', '');
+                var column4 = col[4].replace('"', '');
                 var columna = column.toLowerCase();
                 var columna2 = column2.toLowerCase();
+                var columna4 = column4.toLowerCase();
 
 
-                if ($(texto).val().length >= 2 && columna != valor) {
-                    if (columna.startsWith(valor) || columna2.startsWith(valor)) {
+                if ($(texto).val().length >= 1 && columna != valor) {
+                    if (columna.startsWith(valor) || columna2.startsWith(valor) || columna4.startsWith(valor)) {
                         $(lista).show();
 
                         //$(lista).append('<option value="' + column + '">');
